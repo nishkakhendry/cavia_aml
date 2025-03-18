0ba9e4855c675695d58364fda382e33f1caeb6d0
nishkakhendry <nishka.nkhendry@gmail.com>
2025-03-18 12:06:44 +0000

    arguments for maml 2D navig env

--- rl/arguments.py
+++ rl/arguments.py
@@ -11,8 +11,8 @@ def parse_args():
 
     # General
     parser.add_argument('--env-name', type=str,
-                        default='HalfCheetahDir-v1',
-                        # default='2DNavigation-v0',
+                        # default='HalfCheetahDir-v1',
+                        default='2DNavigation-v0',
                         help='name of the environment')
     parser.add_argument('--gamma', type=float, default=0.95,
                         help='value of the discount factor gamma')
@@ -20,7 +20,7 @@ def parse_args():
                         help='value of the discount factor for GAE')
     parser.add_argument('--first-order', action='store_true',
                         help='use the first-order approximation of MAML/CAVIA')
-    parser.add_argument('--num-context-params', type=int, default=50,                                   # was 2 
+    parser.add_argument('--num-context-params', type=int, default=2,                                   # was 2 / 50
                         help='number of context parameters')
 
     # Run MAML instead of CAVIA
@@ -45,13 +45,13 @@ def parse_args():
     # Task-specific
     parser.add_argument('--fast-batch-size', type=int, default=20,
                         help='number of rollouts for each individual task ()')
-    parser.add_argument('--fast-lr', type=float, default=10,                # was 0.1
+    parser.add_argument('--fast-lr', type=float, default=1.0,                # was 1.0 / 10
                         help='learning rate for the 1-step gradient update of MAML/CAVIA')
 
     # Optimization
     parser.add_argument('--num-batches', type=int, default=500,
                         help='number of batches')
-    parser.add_argument('--meta-batch-size', type=int, default=40,          # was 20
+    parser.add_argument('--meta-batch-size', type=int, default=20,          # was 20 / 40
                         help='number of tasks per batch')
     parser.add_argument('--max-kl', type=float, default=1e-2,
                         help='maximum value for the KL constraint in TRPO')
--- rl/parse_tf.py
+++ rl/parse_tf.py
@@ -15,5 +15,12 @@ if __name__ == "__main__":
     # dir_path = "C:\\Users\\nishk\\OneDrive\\Desktop\\LT\\cavia_aml\\rl\\logs\\HalfCheetahDir-v1\\cavia\\"
     # exp_name = "2_lr=1.0tau=1.0_11_03_2025_17_38_49\\events.out.tfevents.1741714729.e0822e68336f"    
     # parse_tfevent_log(f"{dir_path}/{exp_name}")
-    log_path = "C:\\Users\\nishk\\OneDrive\\Desktop\\LT\\cavia_aml\\rl\\logs\\HalfCheetahDir-v1\\cavia\\50_lr=10tau=1.0_14_03_2025_19_50_07\\events.out.tfevents.1741981807.4390cb3eb32b"
+
+    # HalfCheetahDir-v1 CAVIA
+    # log_path = "C:\\Users\\nishk\\OneDrive\\Desktop\\LT\\cavia_aml\\rl\\logs\\HalfCheetahDir-v1\\cavia\\50_lr=10tau=1.0_14_03_2025_19_50_07\\events.out.tfevents.1741981807.4390cb3eb32b"
+    # HalfCheetahDir-v1 MAML
+    log_path = "C:\\Users\\nishk\\OneDrive\\Desktop\\LT\\cavia_aml\\rl\\logs\\HalfCheetahDir-v1\\maml\\lr=10tau=1.0_16_03_2025_23_02_25\\events.out.tfevents.1742166145.d2d58bd675f2"
+    
+    # # 2dNavigation-v0 CAVIA
+    # log_path = "C:\\Users\\nishk\\OneDrive\\Desktop\\LT\\cavia_aml\\rl\\logs\\2DNavigation-v0_old\\cavia\\2_lr=1.0tau=1.0_13_03_2025_02_20_16\\events.out.tfevents.1741832416.d91585773a5e"
     parse_tfevent_log(log_path)
